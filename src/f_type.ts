2bf5e0ad13fa712d6d2d3139c4d782221c85e1e0
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-06-11 23:50:55 +0800

    类型断言

--- src/f_type.ts
+++ src/f_type.ts
@@ -18,3 +18,10 @@ id({x: '123', y: 23});
 
 // 基元类型
 type InputStr = string;
+
+
+
+// 类型断言 ， 用于指明 返回值的指定类型
+// 这两个是等效的， 类型断言在编译时会被删除， 所以没有与类型断言相关的运行时检测
+// const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElemen
+const myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas")
